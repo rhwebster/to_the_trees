1bff6c5c0b6394fb3428a1a1d14fb2f75a65e942
Ryan Webster <ryan.h.webster@gmail.com>
2022-11-17 12:48:20 -0800

    load

--- frontend/src/store/reservations.js
+++ frontend/src/store/reservations.js
@@ -4,3 +4,15 @@ const LOAD_RESYS = 'reservations/LOAD';
 const ADD_RESY = 'reservation/ADD';
 const DELETE_RESY = 'reservation/DELETE';
 
+const loadResys = (data) => {
+    return {
+        type: LOAD_RESYS,
+        data,
+    };
+};
+
+export const loadReservations = (id) => async (dispatch) => {
+    const allReservations = await fetch(`/api/reservations/user/${id}`);
+    const reservationsArray = await allReservations.json();
+    dispatch(loadResys(reservationsArray));
+};
