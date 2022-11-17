58e3e64a086ec757a709b6b25a3b768d6a7adc4b
Ryan Webster <ryan.h.webster@gmail.com>
2022-11-17 12:57:12 -0800

    add reservations

--- frontend/src/store/reservations.js
+++ frontend/src/store/reservations.js
@@ -1,5 +1,5 @@
 
-import { fetch } from './csrf';
+import { csrfFetch } from './csrf';
 const LOAD_RESYS = 'reservations/LOAD';
 const ADD_RESY = 'reservation/ADD';
 const DELETE_RESY = 'reservation/DELETE';
@@ -11,8 +11,48 @@ const loadResys = (data) => {
     };
 };
 
+const addResy = (data) => {
+    return {
+        type: ADD_RESY,
+        data,
+    };
+};
+
 export const loadReservations = (id) => async (dispatch) => {
     const allReservations = await fetch(`/api/reservations/user/${id}`);
     const reservationsArray = await allReservations.json();
     dispatch(loadResys(reservationsArray));
 };
+
+export const addReservation = (data) => async (dispatch) => {
+    const res = await csrfFetch('/api/reservations/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    const resyData = await res.json();
+    dispatch(addResy(resyData));
+    return resyData;
+};
+
+const initialState = {};
+
+const bookingsReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case LOAD_RESYS: {
+            newState = { ...state };
+            action.data.forEach(booking => {
+                newState[booking.id] = booking;
+            });
+            return newState;
+        }
+        case ADD_RESY: {
+            newState = { ...state };
+            newState[action.data.id] = action.data;
+            return newState;
+        }
+    }
+}
