71c1433bbd50e32139c64179a44d976648afb95f
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-13 11:57:54 -0700

    exclusions

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -41,6 +41,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    }
   });
   return User;
 };
