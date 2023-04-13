4eb1c1f4ddea8653c7ccc1821496fd8c77592dec
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-13 10:31:41 -0700

    csrf test

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('express-async-errors');''
+require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
