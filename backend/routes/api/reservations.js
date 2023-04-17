0eaac7c9b93bcf7c226fa69fdfb2413a35619687
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-17 16:41:10 -0700

    delete resy

--- backend/routes/api/reservations.js
+++ backend/routes/api/reservations.js
@@ -63,4 +63,47 @@ router.put('/:resyId', requireAuth, async (req, res) => {
     });
 
     return res.json(resy);
-})
+});
+
+router.delete('/:resyId', requireAuth, async(req, res) => {
+    const resy = await Reservation.findByPk(resyId {
+        include: {
+            model: Listing
+        }
+    });
+
+    if (!resy) {
+        res.status(404);
+        return res.json({
+            message: "Reservation couldn't be found",
+            statusCode: 404
+        })
+    };
+
+    if ((req.user.id !== resy.Listing.ownerId) && (req.user.id !== resy.guestId)) {
+        res.status(403);
+        return res.json({
+            message: "Unauthorized",
+            statusCode: 403
+        })
+    };
+
+    const today = new Date().toString();
+
+    if (Validator.isAfter(today, resy.startDate.toString())) {
+        res.status(403);
+        return res.json({
+            message: "Reservations cannot be cancelled after they've begun",
+            statusCode: 403
+        })
+    };
+
+    await resy.destroy();
+
+    return res.json({
+        message: "Reservation successfully deleted",
+        statusCode: 200
+    });
+});
+
+module.exports = router;
