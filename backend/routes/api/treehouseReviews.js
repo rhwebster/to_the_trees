310fdf9d697ff0f6296ae4a12b92bacb5e3aa32d
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-17 11:33:28 -0700

    delete review

--- backend/routes/api/treehouseReviews.js
+++ backend/routes/api/treehouseReviews.js
@@ -17,7 +17,7 @@ router.put('/:reviewId', requireAuth, async(req, res) => {
     if (reviewToUpdate.userId !== res.user.id) {
         res.status(403);
         return res.json({
-            message: "Forbidden",
+            message: "Unauthorized",
             statusCode: 403
         });
     };
@@ -32,3 +32,31 @@ router.put('/:reviewId', requireAuth, async(req, res) => {
     return res.json(reviewToUpdate);
 });
 
+router.delete('/:reviewId', requireAuth, async(req, res) => {
+    const review = await TreehouseReview.findByPk(req.params.reviewId);
+
+    if (!review) {
+        res.status(404);
+        return res.json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    if (review.userId !== req.user.id) {
+        res.status(403);
+        return res.json({
+            message: "Unauthorized",
+            statusCode: 403
+        });
+    };
+
+    await review.destroy();
+
+    res.json({
+        message: "Review successfully deleted",
+        statusCode: 200
+    });
+});
+
+module.exports = router;
