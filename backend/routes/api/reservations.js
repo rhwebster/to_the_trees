cd9a407eb22dad9698254458a5197c39b2fd259e
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-17 16:33:20 -0700

    edit resy

--- backend/routes/api/reservations.js
+++ backend/routes/api/reservations.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { requireAuth } = require('../../utils/auth');
 const { User, Listing, Reservation, Image } = require('../../db/models');
+const { Validator } = require('sequelize');
 
 router.get('/', requireAuth, async(req, res) => {
     const resys = await Reservation.findAll({
@@ -25,3 +26,41 @@ router.get('/', requireAuth, async(req, res) => {
     res.json({ Reservations: resys });
 });
 
+router.put('/:resyId', requireAuth, async (req, res) => {
+    const resy = await Reservation.findByPk(resyId);
+
+    if (!resy) {
+        res.status(404);
+        return res.json({
+            message: "Reservation couldn't be found",
+            statusCode: 404
+        })
+    };
+
+    if (req.user.id !== resy.guestId) {
+        res.status(403);
+        return res.json({
+            message: "Unauthorized",
+            statusCode: 403
+        })
+    };
+
+    const { startDate, endDate } = req.body;
+
+    const today = new Date().toString();
+
+    if (Validator.isAfter(today, resy.startDate.toString())) {
+        res.status(403);
+        return res.json({
+            message: "Reservations cannot be modified after they begin",
+            statusCode: 403
+        })
+    };
+
+    await resy.update({
+        startDate: startDate,
+        endDate: endDate
+    });
+
+    return res.json(resy);
+})
