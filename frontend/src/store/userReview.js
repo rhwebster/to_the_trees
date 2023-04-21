e61e4188632b154ddaaacc46d1228f3103368957
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-21 15:18:15 -0700

    user review reducer

--- frontend/src/store/userReview.js
+++ frontend/src/store/userReview.js
@@ -10,6 +10,13 @@ const loadUserReviews = (data) => {
     }
 };
 
+const deleteReview = (id) => {
+    return {
+        type: DELETE_REVIEW,
+        id
+    }
+}
+
 export const getUserReviews = (userId) => async (dispatch) => {
     const res = await csrfFetch(`/api/users/${userId}/reviews`);
 
@@ -23,6 +30,53 @@ export const getUserReviews = (userId) => async (dispatch) => {
     }
 };
 
+export const addGuestReview = (userId, data) => async (dispatch) => {
+    const res = await csrfFetch(`/api/guestReviews/`, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+        const reviewData = await res.json();
+        dispatch(loadUserReviews(userId));
+        return reviewData;
+    } else {
+        const err = res.json();
+        return err;
+    }
+};
+
+export const editGuestReview = (userId, data) => async (dispatch) => {
+    const res = await csrfFetch(`/api/guestReviews/${data.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+        const reviewData = await res.json();
+        dispatch(getUserReviews(userId));
+        return reviewData;
+    } else {
+        const err = await res.json();
+        return err;
+    }
+}
+
+export const deleteUserReview = (id) => async (dispatch) => {
+    const res = await csrfFetch(`/api/guestReviews/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (res.ok) {
+        const deleted = await res.json();
+        dispatch(deleteReview(id));
+        return deleted;
+    } else {
+        const err = await res.json();
+        return err;
+    }
+}
+
 const initialState = { userReviews: {} };
 
 const guestReviewReducer = (state = initialState, action) => {
@@ -30,6 +84,11 @@ const guestReviewReducer = (state = initialState, action) => {
         case USER_REVIEWS: {
             const newState = {};
             newState.userReviews = action.data;
+            return newState;
+        }
+        case DELETE_REVIEW: {
+            const newState = { ...state }
+            delete newState.userReviews[action.id];
             return newState;
         }
         default: return state;
