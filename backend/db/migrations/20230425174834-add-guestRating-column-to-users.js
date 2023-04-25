66501797b886c61b13dcf8a2697b90922f240021
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-25 10:51:25 -0700

    add numReviews and guestRating to users

---
+++ backend/db/migrations/20230425174834-add-guestRating-column-to-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'Users',
+      'guestRating',
+      {
+        type: Sequelize.FLOAT,
+        defaultValue: null
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'guestRating');
+  }
+};
