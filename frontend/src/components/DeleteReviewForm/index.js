a711c65fcad4077aafbf3371229bc8d6a3fbfbc7
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-26 16:03:30 -0700

    delete review form

--- frontend/src/components/DeleteReviewForm/index.js
+++ frontend/src/components/DeleteReviewForm/index.js
@@ -8,7 +8,35 @@ import { getOneListing } from "../../store/listing";
 import './index.css';
 
 const DeleteReviewForm = () => {
+    const dispatch = useDispatch();
 
+    const deleteReview = () => {
+        dispatch(deleteTreehouseReview(reviewId))
+            .then(() => {
+                dispatch(getOneListing(listingId))})
+            .then(setShowDeleteForm(false));
+    }
+
+    return (
+        <div className="delete-review-holder">
+            <div className="delete-review signup-exit-holder">
+                <div onClick={() => setShowDeleteForm(false)} className="circle delete-review">
+                <i id="exit-reviews" className="fa-solid fa-xmark"></i>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h4>Delete This Review?</h4>
+                </div>
+                <div className="delete-review-bottom-nav">
+                    <div>
+                        {"<"}<Link className="underline" onClick={() => setShowDeleteForm(false)}>Back</Link>
+                    </div>
+                    <button className="delete-review-button" onClick={deleteReview}>Delete Review</button>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default DeleteReviewForm;
