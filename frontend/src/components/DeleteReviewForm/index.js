f86ab700b59e4be51bd4c91d33e7a12ce254fcc0
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-26 16:05:10 -0700

    remove unnecessary thunk

--- frontend/src/components/DeleteReviewForm/index.js
+++ frontend/src/components/DeleteReviewForm/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { deleteTreehouseReview, getTreehouseReviews } from "../../store/treehouseReview";
+import { deleteTreehouseReview } from "../../store/treehouseReview";
 import { getOneListing } from "../../store/listing";
 
 import './index.css';
