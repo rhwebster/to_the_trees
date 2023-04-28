c9d60b8d1a35f37c3b2454b5d2490e667b7b4a0f
Ryan Webster <ryan.h.webster@gmail.com>
2023-04-28 10:42:07 -0700

    refactor error handlers

--- frontend/src/components/CreateListingForm/index.js
+++ frontend/src/components/CreateListingForm/index.js
@@ -23,35 +23,47 @@ const CreateListing = () => {
     const [url1, setUrl1] = useState('');
     const [files, setFiles] = useState([]);
     const [imageLoading, setImageLoading] = useState(false);
+    const [addressErrors, setAddressErrors] = useState([]);
+    const [photoErrors, setPhotoErrors] = useState([]);
+    const [desciptionErrors, setDescriptionErrors] = useState([]);
+    const [pricePerNightErrors, setPricePerNightErrors] = useState([]);
 
-    const [listingErrors, setListingErrors] = useState([]);
     const checker = (param) => {
         if (!param || param.length > 255) return true;
         else return false;
     }
 
-    const handleListingErrors = () => {
+    const handleAddressErrors = () => {
         let errors = [];
-        if (!name) errors.push('Need Treehouse Name');
-        if (name.length > 255) errors.push('Treehouse Name is Too Long');
         if (checker(address)) errors.push('Please enter valid address');
         if (checker(cityState)) errors.push('Please enter valid City and State');
-        if (checker(desciption)) errors.push('Please enter a description between 1 and 255 characters');
-        if (pricePerNight < 1 || pricePerNight > 10000 || isNaN(pricePerNight)) errors.push('Please enter a valid price (less than $10,000');
-        if (maxGuests < 1 || isNaN(maxGuests)) errors.push('Please enter a valid number of guests');
-        if (lat > 90 || lat < -90 || isNaN(lat)) errors.push('Please enter a valid latitude');
-        if (lon > 180 || lon < -180 || isNaN(lon)) errors.push('Please enter a valid longitude');
-        setListingErrors(errors);
+        setAddressErrors(errors);
         return errors;
     }
 
-    const handlePhotos = () => {
+    const handlePhotoErrors = () => {
         let errors = [];
         if (!images.length) errors.push('Please upload at least one image')
         if (images.length > 10) errors.push('The maximum number of images you can upload is 10');
-        setListingErrors(errors);
+        setPhotoErrors(errors);
         return errors;
     };
+
+    const handlePriceErrors = () => {
+        let errors = [];
+        if (isNaN(pricePerNight) || pricePerNight < 1) errors.push('Please provide a price for your treehouse');
+        if (pricePerNight > 10000) errors.push('Please list your treehouse at less than $10,000/night');
+        return errors;
+    }
+
+    const handleNameAndDescriptionErrors = () => {
+        let errors = [];
+        if (!name.length) errors.push('Please provide a valid name');
+        if (name.length > 49) errors.push('Please choose a shorter name');
+        if (desciption.length > 255) errors.push('Description must be 255 characters or less');
+        if (!desciption.length) errors.push('Please provide a description');
+        return errors;
+    }
 
     const resetState = () => {
         setName('');
@@ -97,13 +109,24 @@ const CreateListing = () => {
         }
     }
 
-    const incrementPrice = (e) => {
-        setPricePerNight(Number(pricePerNight) + 5);
+    const incrementButton = (e) => {
+        if (e.target.value === pricePerNight) {
+            setPricePerNight(Number(pricePerNight) + 5);
+        }
+        if (e.target.value === maxGuests) {
+            setMaxGuests(Number(maxGuests) + 1);
+        }
     };
-    const decrementPrice = (e) => {
-        if (!pricePerNight) return
-        if (pricePerNight <= 5) setPricePerNight(0);
-        if (pricePerNight > 5) setPricePerNight(Number(pricePerNight) - 5);
+
+    const decrementButton = (e) => {
+        if (e.target.value === pricePerNight) {
+            if (!pricePerNight || pricePerNight <= 5) setPricePerNight(0);
+            if (pricePerNight > 5) setPricePerNight(Number(pricePerNight) - 5);
+        }
+        if (e.target.value === maxGuests) {
+            if (!maxGuests || maxGuests <= 1) setMaxGuests(0);
+            if (maxGuests > 1) setMaxGuests(Number(pricePerNight) - 1);
+        }
     }
 
     const update = (e) => {
@@ -116,4 +139,38 @@ const CreateListing = () => {
         }
         setFiles(images)
     };
+
+    return (
+        <div className="create-spot-exterior-flex">
+            <div className="create-spot-exterior-holder">
+                <h2>Host Your Treehouse</h2>
+                <Link className="underline" to='/'>Cancel</Link>
+                <div className="form-holder">
+                    <form className="create-new-listing-form" onSubmit={handleSubmit}>
+                        <div className="location-exterior">
+                            <h4 className="form-instructions">Treehouse Location</h4>
+                            <div className="location-form">
+                                <div className="input">
+                                    <label className="address-label" htmlFor="address">Address</label>
+                                    <input className="address-input" placeholder="Street Address..." id="address" type="text" value={address} onChange={(e) => setAddress(e.target.value)}></input>
+                                </div>
+                                <div className="input">
+                                    <label className="cityState-label" htmlFor="cityState">City and State</label>
+                                    <input className="address-input" placeholder="City, State..." id="cityState" type="text" value={cityState} onChange={(e) => setCityState(e.target.value)}></input>
+                                </div>
+                            </div>
+                            {listingErrors.length > 0 && (
+                                <div className="errors">
+                                    <i className="fa-solid fa-circle fa-cicle-exclamation"></i>Please fill in required fields:
+                                    <ul>
+                                        {listingErrors.map(err => <li key={err}>{err}</li>)}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
