515ef05384b5141629e6ff9bed86dcd61840327f
Ryan Webster <ryan.h.webster@gmail.com>
2023-05-03 11:25:22 -0700

    resy instructions

--- frontend/src/components/DeleteReservationForm/index.js
+++ frontend/src/components/DeleteReservationForm/index.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-
-import { deleteTreehouseReview, getTreehouseReviews } from "../../store/treehouseReview";
-import { getOneListing } from "../../store/listing";
 
 import './index.css';
 
---
+++ frontend/src/components/EditReservation/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Calendar from 'react-calendar';
+
+import
+import EditConfirmation from './confirmation';
